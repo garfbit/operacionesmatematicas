a0f242fd2413ccf0f0392b721ecaa662795b577a
garfbit <104676660+garfbit@users.noreply.github.com>
2025-08-28 05:56:06 -0500

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,9 @@
-
+let numero3 = 0
+basic.forever(function () {
+    let numero2 = 0
+    let numero1 = 0
+    numero3 = 0
+    numero3 = 0
+    numero3 = 0
+    basic.showNumber(numero1 + (numero2 + numero3))
+})
